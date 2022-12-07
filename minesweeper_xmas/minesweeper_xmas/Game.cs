3af6c88a0af6fdc9ab6bc9dfc101e37109854d02
IaamSteve <kerekesistvan@turr.hu>
2022-12-07 10:34:49 +0100

    2d tömb generálás 1/2

--- minesweeper_xmas/minesweeper_xmas/Game.cs
+++ minesweeper_xmas/minesweeper_xmas/Game.cs
@@ -12,9 +12,21 @@ namespace minesweeper_xmas
 {
     public partial class Game : Form
     {
+        
         public Game()
         {
             InitializeComponent();
+            int magassag = 0;
+            int szelesseg = 0;
+            int[,] matrix = new int[magassag,szelesseg];
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+
+                }
+            }
         }
     }
 }
